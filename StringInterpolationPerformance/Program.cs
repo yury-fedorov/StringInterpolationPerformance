3ccfeb9375bb89bf48c699446b1e549addf522a6
Yury Fedorov <yury.fedorov@gmail.com>
2019-07-30 00:13:41 +0200

    Refactoring to run on .NET Core 2.2. Corrected bug with not identical functionality in different test cases.

--- StringInterpolationPerformance/Program.cs
+++ StringInterpolationPerformance/Program.cs
@@ -3,32 +3,18 @@ using BenchmarkDotNet.Running;
 using System;
 using System.Text;
 
-namespace StringInterpolationPerformanceB
+namespace StringInterpolationPerformance
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run<StringFormatVsStringInterpolation>();
-            
-            summary = BenchmarkRunner.Run<StringConcatVsStringInterpolation>();
-            
-            summary = BenchmarkRunner.Run<StringBuilderVsStringInterpolation>();
-        }
+        static void Main(string[] args) => BenchmarkRunner.Run<StringFormatTest>();
     }
 
-    public class StringFormatVsStringInterpolation
+    public class StringFormatTest
     {
-        private string data1;
-        private string data2;
-        private string data3;
-
-        public StringFormatVsStringInterpolation()
-        {
-            data1 = Guid.NewGuid().ToString();
-            data2 = Guid.NewGuid().ToString();
-            data3 = Guid.NewGuid().ToString();
-        }
+        private readonly string data1 = Guid.NewGuid().ToString();
+        private readonly string data2 = Guid.NewGuid().ToString();
+        private readonly string data3 = Guid.NewGuid().ToString();
 
         [Benchmark]
         public string StringFormat() => string.Format("test format{0}, {1}, {2}", data1, data2, data3); 
@@ -36,48 +22,13 @@ namespace StringInterpolationPerformanceB
         [Benchmark]
         public string StringInterpolation() => ($"test format{data1}, {data2}, {data3}");
 
+        [Benchmark]
+        public string StringConcat() => "test format" + data1 + ", " + data2 + ", " + data3;
+
+        [Benchmark]
+        public string StringBuilder() => (new StringBuilder()).Append("test format")
+		.Append(data1).Append(", ")
+		.Append(data2).Append(", ")
+		.Append(data3).ToString();
     }
-
-    public class StringConcatVsStringInterpolation
-    {
-        private string data1;
-        private string data2;
-        private string data3;
-        
-        public StringConcatVsStringInterpolation()
-        {
-            data1 = Guid.NewGuid().ToString();
-            data2 = Guid.NewGuid().ToString();
-            data3 = Guid.NewGuid().ToString();
-        }
-
-        [Benchmark]
-        public string StringConcat() => "test format" + data1 + data2 + data3;
-
-        [Benchmark]
-        public string StringInterpolation() => ($"test format{data1}, {data2}, {data3}");
-
-    }
-
-    public class StringBuilderVsStringInterpolation
-    {
-        private string data1;
-        private string data2;
-        private string data3;
-
-        public StringBuilderVsStringInterpolation()
-        {
-            data1 = Guid.NewGuid().ToString();
-            data2 = Guid.NewGuid().ToString();
-            data3 = Guid.NewGuid().ToString();
-        }
-
-        [Benchmark]
-        public string StringBuilder() => (new StringBuilder()).Append("test format").Append(data1).Append(data2).Append(data3).ToString();
-
-        [Benchmark]
-        public string StringInterpolation() => ($"test format{data1}, {data2}, {data3}");
-
-    }
-
 }
